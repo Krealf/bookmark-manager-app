951f139fdbd33d44996fc1a47549c92e8e6c7854
krilph <krealf@mail.ru>
2026-04-25 14:11:44 +0300

    fix: added Vite types in vite-env.d.ts

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -8,3 +8,11 @@ declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
